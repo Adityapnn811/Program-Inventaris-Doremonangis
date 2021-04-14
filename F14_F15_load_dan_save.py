775afcf3796bf0273fec70f04e6f9c02534a7b54
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2021-04-14 15:34:49 +0700

    Ngehapus fungsi login dari fungsi save.py

--- F14_F15_load_dan_save.py
+++ F14_F15_load_dan_save.py
@@ -153,34 +153,5 @@ def save():
 
     print("Data telah disimpan pada folder " + nama_folder + "!")
 
-
-# FUNGSI VALIDASI LOGIN
-def validate_login(username, password, database):
-    validate = False
-    for data in database:
-        if data[1] == username and data[4] == password:
-            validate = True
-    if validate:
-        print("Halo {}! Selamat datang di kantong ajaib".format(username))
-        return True
-    else:
-        print("Username/Password salah! Silahkan coba lagi")
-        return False
-
-#FUNGSI LOGIN (YANG DIPANGGIL DI MAIN)
-def login(database):
-    username = input("Username: ")
-    password = input("Password: ")
-    while validate_login(username, password, database) == False:
-        username = input("Username: ")
-        password = input("Password: ")
-        validate_login(username, password, database)
-    # username id buat tau siapa yang login
-    for i in range(len(database)):
-        if database[i][1] == username:
-            userid = database[i][0]
-    return userid
-
-
 if __name__ == '__main__':
     load()
