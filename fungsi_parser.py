70351521c9f22095ec14e8319c9cdac31faad173
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2021-04-28 15:24:50 +0700

    Nambahin parser

--- fungsi_parser.py
+++ fungsi_parser.py
@@ -10,3 +10,19 @@ def parser(line):
             string_group += i
     line_copy.append(str(string_group))
     return line_copy
+
+def parser_tanggal(tanggal_str):
+    tanggal_copy = []
+    string_group = ""
+    for i in tanggal_str:
+        if i == "/":
+            tanggal_copy += ""
+            tanggal_copy.append(str(string_group))
+            string_group = ""
+        else:
+            string_group += i
+    tanggal_copy.append(string_group)
+    # Ubah array tanggal ke integer
+    for j in range(len(tanggal_copy)):
+        tanggal_copy[j] = int(tanggal_copy[j])
+    return tanggal_copy
